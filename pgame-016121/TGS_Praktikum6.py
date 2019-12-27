655e7064056adc1b8d2b9789091642a6e9a212ef
misnuhanafi1234 <57500287+misnuhanafi1234@users.noreply.github.com>
2019-12-28 02:43:24 +0900

    Add files via upload

---
+++ pgame-016121/TGS_Praktikum6.py
@@ -0,0 +1,18 @@
+batas='*'*30
+c = 1 
+while c < 6: 
+	print("C == ",c) 
+	c += 1 
+else:
+	print("batas c < 6")
+print(batas)
+
+
+loyang = ["Apel", "Pisang", "Durian", "Melon"] 
+for buah in loyang:
+	print(buah) 
+
+
+print(batas)
+for x in range (5) :
+	print("Hallo Anak Info")
---
+++ pgame-016121/latihan.py
@@ -0,0 +1,26 @@
+# # Tugas_code6 : Menggunakan Kondisional
+# nilai = int (input("MASUKAN NILAI :"))
+# if nilai >= 80:		# menggunakan kondisional if
+#     print ("Sangat Baik")
+
+# nilai >= 80
+# if nilai >= 65 :		# menggunakan kondisional if
+#     print ("Cukup Baik")
+
+# nilai >= 65
+# if nilai >= 50 :		# menggunakan kondisional if
+#     print ("Baik")
+
+# nilai >= 30
+# if nilai >= 25 :		# menggunakan kondisional if
+#     print ("Kurang")
+
+nilai = int (input("MASUKAN NILAI :"))
+if nilai >= 80:
+	print ("Sangat Baik")
+elif nilai >= 65:
+	print ("Cukup Baik")
+elif nilai >= 50:
+	print ("Baik")
+else:
+	print ("Kurang")
